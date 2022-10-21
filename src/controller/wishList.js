1c5e543e506648d968c78202f3bb981c770c4047
sam <yifang.sun@lianwei.com.cn>
2022-10-21 14:57:19 +0800

    新建心愿单

--- src/controller/wishList.js
+++ src/controller/wishList.js
@@ -4,8 +4,19 @@ const { dealToken } = require("./revertToken2UserId.js");
 const insertWishList = async (req) => {
   try {
     let user_id = await dealToken(req.headers.token);
+
     if (user_id) {
-      console.log(user_id);
+      const { title, wishCoin, wishListLink, wishListImg, wishListContent } =
+        req.body;
+      const insertWishListSql = `
+      insert into wish_list (title, wish_prise,wish_link,wish_img_id,description) 
+      values('${title}', '${wishCoin}', '${wishListLink}','${wishListImg}','${wishListContent}')
+      `;
+      return exec(insertWishListSql).then((insertData) => {
+        if (insertData) {
+          return "success";
+        }
+      });
     }
   } catch (error) {
     console.log(error);
